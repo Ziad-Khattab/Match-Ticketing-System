d0c125d57420bcc1436801850b527443dd5886ba
Mahmoud Salah <mahmoudalmasry0005@gmail.com>
2026-01-06 01:25:27 +0200

    chore:Implement Ticket and Match source files

---
+++ Match.cpp
@@ -0,0 +1,44 @@
+#include "Match.h"
+
+Match::Match(int id, string code, int cap) {
+    this->match_id = id;
+    this->matchCode = code;
+    this->capacity = cap;
+    this->bookedCount = 0;
+}
+
+bool Match::isFull() {
+    return bookedCount >= capacity;
+}
+
+string Match::getMatchCode() {
+    return matchCode;
+}
+
+bool Match::bookSeat(string fanName) {
+    if (isFull())
+        return false;
+
+    int ticketId = tickets.size() + 1;
+    Ticket t(ticketId, match_id, fanName);
+    tickets.push_back(t);
+    bookedCount++;
+    return true;
+}
+
+void Match::showTickets() {
+    if (tickets.empty()) {
+        cout << "No tickets booked for this match.\n";
+        return;
+    }
+
+    for (int i = 0; i < tickets.size(); i++) {
+        cout << "Ticket ID: " << tickets[i].getTicketId()
+             << " | Owner: " << tickets[i].getOwnerName() << endl;
+    }
+}
+
+void Match::showAvailableSeats() {
+    cout << match_id << " - " << matchCode
+         << " | Available: " << (capacity - bookedCount) << endl;
+}
